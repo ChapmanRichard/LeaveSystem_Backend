6e887dc5885ad564800dad13d624916f09cb792d
Kong <jiangsimpler@gmail.com>
2026-06-10 16:11:59 +0800

    chore: reset backend to training skeleton

--- Api.BLL/CommandDecorators/ActionNameCommandDecorator.cs
+++ Api.BLL/CommandDecorators/ActionNameCommandDecorator.cs
@@ -1,13 +1,5 @@
 ﻿using Api.Common.Commands;
-using Api.Common.DI;
-using Api.Contract.Commands;
-using Api.Contract.Constants;
-using Api.Contract.DTO;
-using Api.Contract.Queries;
-using Api.Contract.ServiceInterfaces;
-using Api.DAL;
 using System;
-using System.Linq;
 
 namespace Api.BLL.CommandDecorators
 {
@@ -23,16 +15,11 @@ namespace Api.BLL.CommandDecorators
 
     public class ActionNameCommandHandlerDecorator<TCommand, TResult> : ICommandHandler<TCommand, TResult> where TCommand : ICommand<TResult>
     {
-
-        private ActionNameProvider actionNameProvider;
-        private UnitOfWorkLeaveSystem unitOfWork;
-
         private readonly ICommandHandler<TCommand, TResult> handler;
-        public ActionNameCommandHandlerDecorator(ICommandHandler<TCommand, TResult> handler, ActionNameProvider actionNameProvider, UnitOfWorkLeaveSystem unitOfWork)
+
+        public ActionNameCommandHandlerDecorator(ICommandHandler<TCommand, TResult> handler)
         {
-            this.handler = handler;
-            this.actionNameProvider = actionNameProvider;
-            this.unitOfWork = unitOfWork;
+            this.handler = handler ?? throw new ArgumentNullException(nameof(handler));
         }
 
         public TResult Handle(TCommand cmd)
--- Api.DAL/LeaveSystemDBContext.cs
+++ Api.DAL/LeaveSystemDBContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Api.Contract.Model;
 using Microsoft.Extensions.Configuration;
 
 namespace Api.DAL
@@ -41,50 +40,6 @@ namespace Api.DAL
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<User>(entity =>
-            {
-                entity.Property(x => x.Username).HasMaxLength(50).IsRequired();
-                entity.Property(x => x.Role).HasMaxLength(20).IsRequired();
-            });
-
-            modelBuilder.Entity<LeaveQuota>(entity =>
-            {
-                entity.Property(x => x.LeaveType).HasMaxLength(20).IsRequired();
-                entity.Property(x => x.TotalDays).HasPrecision(5, 1);
-                entity.Property(x => x.RemainingDays).HasPrecision(5, 1);
-                //entity.Property(x => x.UpdatedAt).HasDefaultValueSql("CURRENT_TIMESTAMP");
-
-                entity.HasOne(x => x.User)
-                    .WithMany(x => x.LeaveQuotas)
-                    .HasForeignKey(x => x.UserId)
-                    .OnDelete(DeleteBehavior.Cascade);
-            });
-
-            modelBuilder.Entity<LeaveRequest>(entity =>
-            {
-                entity.Property(x => x.LeaveType).HasMaxLength(20).IsRequired();
-                entity.Property(x => x.Reason).HasMaxLength(500).IsRequired();
-                entity.Property(x => x.Status).HasMaxLength(20).IsRequired();
-                entity.Property(x => x.ApprovalComment).HasMaxLength(500);
-                entity.Property(x => x.Duration).HasPrecision(5, 1);
-                //entity.Property(x => x.CreatedAt).HasDefaultValueSql("CURRENT_TIMESTAMP");
-                //entity.Property(x => x.UpdatedAt).HasDefaultValueSql("CURRENT_TIMESTAMP");
-
-                entity.HasOne(x => x.Applicant)
-                    .WithMany(x => x.SubmittedLeaveRequests)
-                    .HasForeignKey(x => x.ApplicantId)
-                    .OnDelete(DeleteBehavior.Restrict);
-
-                entity.HasOne(x => x.Approver)
-                    .WithMany(x => x.ApprovedLeaveRequests)
-                    .HasForeignKey(x => x.ApproverId)
-                    .OnDelete(DeleteBehavior.Restrict);
-            });
         }
-
-        public DbSet<User> Users { get; set; }
-        public DbSet<LeaveQuota> LeaveQuotas { get; set; }
-        public DbSet<LeaveRequest> LeaveRequests { get; set; }
     }
 }
